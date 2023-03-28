f3a5b978d914b711f8341d910134536be5637d54
retr0 <retr0lbb@gmail.com>
2023-03-28 17:55:07 -0300

    update elements

--- main.js
+++ main.js
@@ -1,29 +1,30 @@
 const input = document.getElementById('palavra')
 const btn = document.querySelector('#btn')
 
-const frames = []
-const coluns= 4
-const palavras = ["algos", "mamae", "sugar", "viver"]
-const palavraSelecionada = palavras[gerarNumeroAleatorio(0, 3)];
+
+
+const frames = [[], [], [], [], [], []]
+const palavras = ["algos", "mamae", "sugar", "viver","trans","atomo","china"]
+const palavraSelecionada = palavras[gerarNumeroAleatorio(0, 6)];
+var colluns = 0
 
 console.log(palavraSelecionada)
 
 console.log(frames)
+console.log(input)
 
 btn.addEventListener('click',() => {
-    if(input.length =5){
-        console.log(input.value)
-        for(j=0; j<coluns; j++){
-            frames[i] = []
-            for(i=0; i<5; i++){
-                frames[j][i] = document.getElementById(`${j+1}_${i+1}`)
-                console.log(frames)
+    if(input.value.length  ===5|| colluns >6){
+            for(j=0; j<5; j++){
+                if(input.value ){
+                frames[colluns][j] = document.getElementById(`${colluns}_${j}`);
+                printTela()
+                }
             }
-            
-        }
-        
+        console.log(frames)
+        colluns += 1;
     }else{
-        return console.log("palavra invalida")
+        return console.log("palavra invalida "+ length + input.value.length); 
     }
 
 })
@@ -32,6 +33,19 @@ function gerarNumeroAleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
+ 
+const printTela = () =>{
+    frames[colluns][j].innerHTML = `${input.value[j]}`
+
+    //checka para ver se a letra esta na posição correta
+    if(palavraSelecionada[j]== `${input.value[j]}`){
+       frames[colluns][j].className ='a'
+    }else if(palavraSelecionada.indexOf(input.value[j]) !== -1){
+        frames[colluns][j].className ='b'
+
+    }
+
+}
 
   //coloacar dentro do for
   //frames[j][i].innerHTML = `${input.value[i]}`
